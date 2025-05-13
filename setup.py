55d6b10073ef1d9f7db12639beda86a32ddb90f1
yotaenom <yotaro.enomoto@student.ie.edu>
2025-05-13 17:35:34 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ml_ops project by group_5',
+    author='group_5',
+    license='MIT',
+)
